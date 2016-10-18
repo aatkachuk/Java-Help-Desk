f080e2ec168c959aff472ce9309a8c4e5896524b
Alex <a.tkachuk@ng-serv.com>
2016-10-18 16:36:49 +0300

    зшг

--- Sample_java/src/dick.java
+++ Sample_java/src/dick.java
@@ -1,13 +1,9 @@
-import javax.swing.JFrame;
+import java.util.Scanner;
 
 public class dick {
 	public static void main(String agrs[]){
-		reader r = new reader("Counter");
-		r.setVisible(true);
-		r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		r.setSize(200,300);
-		r.setResizable(false);
-		r.setLocationRelativeTo(null);
+
+		
 		
 	}
 
