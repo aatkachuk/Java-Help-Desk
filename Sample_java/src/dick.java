0fe3c6300f77187ee72a05db783ff810adfc2b90
Alex <a.tkachuk@ng-serv.com>
2016-10-18 17:02:54 +0300

    New format

--- Sample_java/src/dick.java
+++ Sample_java/src/dick.java
@@ -1,10 +1,43 @@
 import java.util.Scanner;
 
-public class dick {
-	public static void main(String agrs[]){
+import javax.swing.JOptionPane;
 
+import java.io.File;
+
+
+public class dick {
+	static String m [][]= new String[5][3];
+	static Scanner scn;
+	
+	public static void main(String agrs[]){
+			
+			openFile();
+			readFile();
+			out();
 		
 		
+	}
+	private static void out() {
+		for( int row=0;row<m.length;row++){
+			for(int col=0;col<m[row].length;col++){
+				System.out.print(m[row][col] + "  ");
+			} System.out.println();
+		}
+		
+	}
+	private static void readFile() {
+		while(scn.hasNext()){
+			for( int row=0;row<m.length;row++){
+				for(int col=0;col<m[row].length;col++){
+					m[row][col] = scn.next();
+				}
+			}
+		}
+		
+	}
+	private static void openFile() {
+		try{scn = new Scanner(new File ("res//1.txt"));
+		}catch(Exception e){JOptionPane.showMessageDialog(null, "No file was found!");}
 	}
 
 }
